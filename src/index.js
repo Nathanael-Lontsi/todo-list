f04f52ea145059c47ffba0a23bc1f36aac35f31e
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-01-27 23:57:47 +0100

    modifications made

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/styles.css';
 
-const input = document.getElementById('myInput');
+/* const input = document.getElementById('myInput');
 const add = document.querySelector('.addBtn');
 const list = document.querySelector('.myList');
 
@@ -10,4 +10,48 @@ function todoList() {
 }
 add.addEventListener('click', () => {
   todoList();
+}); */
+
+// OUR TODOS
+let todosArr = [
+  { value: "write articles" },
+  { value: "code" },
+  { value: "play guitar" },
+  { value: "read" },
+];
+
+// GRAB OUR span and ul from DOM
+let todoList = document.getElementById("todoList");
+let noOfTodos = document.getElementById("noOfTodos");
+
+// A FUNCTION TO CREATE TODO
+const createTodo = (arr) => {
+  todoList.innerHTML = "";
+  arr.forEach((todo, id) => {
+    let list = document.createElement("li");
+    let spanWithValue = document.createElement("span");
+    let spanWithBtn = document.createElement("button");
+
+    spanWithValue.innerText = todo.value;
+    spanWithBtn.innerText = "X";
+    spanWithBtn.style.color = "red";
+
+    spanWithBtn.addEventListener("click", () => {
+      deleteTodo(id);
+    });
+
+    list.appendChild(spanWithValue);
+    list.appendChild(spanWithBtn);
+
+    todoList.appendChild(list);
+  });
+  noOfTodos.innerText = todosArr.length;
+};
+
+const deleteTodo = (index) => {
+  todosArr.splice(index, 1);
+  createTodo(todosArr);
+};
+window.addEventListener("load", () => {
+  createTodo(todosArr);
 });
